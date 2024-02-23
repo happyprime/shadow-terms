f002e12693339ffeb445798670c5a044ae0662af
Jeremy Felt <jeremy.felt@gmail.com>
2024-02-23 11:56:44 -0800

    Update return documentation in tests, use `factory()`

--- tests/test-taxonomy-registration.php
+++ tests/test-taxonomy-registration.php
@@ -15,7 +15,7 @@ class TestTaxonomyRegistration extends WP_UnitTestCase {
 	 * Test a post type that has declared support for shadow terms in
 	 * the register_post_type() arguments.
 	 */
-	public function test_post_type_with_register_post_type_support_registers_shadow_taxonomy() {
+	public function test_post_type_with_register_post_type_support_registers_shadow_taxonomy(): void {
 		$this->assertTrue( taxonomy_exists( 'example_connect' ) );
 	}
 
@@ -23,14 +23,14 @@ class TestTaxonomyRegistration extends WP_UnitTestCase {
 	 * Test a post type that has declared support for shadow terms in
 	 * add_post_type_support().
 	 */
-	public function test_post_type_with_add_post_type_support_registers_shadow_taxonomy() {
+	public function test_post_type_with_add_post_type_support_registers_shadow_taxonomy(): void {
 		$this->assertTrue( taxonomy_exists( 'another-example_connect' ) );
 	}
 
 	/**
 	 * Test a post type that has not declared support for shadow terms.
 	 */
-	public function test_post_type_with_no_support_has_no_shadow_taxonomy() {
+	public function test_post_type_with_no_support_has_no_shadow_taxonomy(): void {
 		$this->assertFalse( taxonomy_exists( 'unexample_connect' ) );
 	}
 }
